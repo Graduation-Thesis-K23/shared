e19daf39ddcc6122c3ee41bd9978e5aa81806349
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-06-17 15:49:49 +0700

    feat: categories list

---
+++ categories.ts
@@ -0,0 +1,45 @@
+// view with ../../sub-categories
+// sub-categories is submodule
+
+export default [
+  {
+    id: 1,
+    textKey: "categories.springRolls",
+  },
+  {
+    id: 2,
+    textKey: "categories.toiletPaper",
+  },
+  {
+    id: 3,
+    textKey: "categories.cream",
+  },
+  {
+    id: 4,
+    textKey: "categories.noodles",
+  },
+  {
+    id: 5,
+    textKey: "categories.washingPowder",
+  },
+  {
+    id: 6,
+    textKey: "categories.fishSauce",
+  },
+  {
+    id: 7,
+    textKey: "categories.softDrinks",
+  },
+  {
+    id: 8,
+    textKey: "categories.vegetables",
+  },
+  {
+    id: 9,
+    textKey: "categories.milk",
+  },
+  {
+    id: 10,
+    textKey: "categories.pork",
+  },
+];
