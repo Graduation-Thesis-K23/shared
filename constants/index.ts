5377ea5981b26e99c5a9a0c72d7c213c0926e5c9
Hiep Nguyen <hiepnguyen6014@gmail.com>
2023-07-27 12:40:27 +0700

    update

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,6 @@
 export const USER_ROLE = "USER";
+export const BRANCH_ROLE = "BRANCH";
+export const ADMIN_ROLE = "ADMIN";
+export const EMPLOYEE_ROLE = "EMPLOYEE";
+export const SHIPPER_ROLE = "SHIPPER";
+
--- enums.ts
+++ enums.ts
@@ -1,7 +1,6 @@
 export enum Role {
   Admin = "ADMIN",
   Employee = "EMPLOYEE",
-  Manager = "MANAGER",
   Shipper = "SHIPPER",
   Branch = "BRANCH",
 }
